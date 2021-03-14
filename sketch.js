cb5e9884cd792ec7103507816989c37e1a80dddb
Kenji <70079754+OkayKenji@users.noreply.github.com>
2021-03-13 22:49:30 -0500

    Updates
    text will now change when it hits the edge. Made it easier to change the text thats going to be displayed.

--- sketch.js
+++ sketch.js
@@ -1,6 +1,12 @@
 /**
- * Credit to p5.js and the Coding Train. 
+ * Credit to p5.js & Daniel Shiefman of Coding Train for teaching me some of the 
+ * concepts seen here. (though this project was devloped indepently)
+ * 
+ * And to whoever invented the bouncing DVD logo. 
  */
+
+let nameToDisplay = "nameHere"; //edit this line
+let small = 0; //set to 0 to get random colors, 255 to get white only
 
 let myFont;
 let x;
@@ -9,18 +15,21 @@ let addx = true;
 let addy = true;
 let ratex;
 let ratey; 
+let crickets = false;
+let count = 0; 
+
 
 //loads font
 function preload() {
-  myFont = loadFont('https://raw.githubusercontent.com/OkayKenji/ZoomProject/master/Lato-Bold.ttf');
-
+  //myFont = loadFont('https://raw.githubusercontent.com/OkayKenji/ZoomProject/master/Lato-Bold.ttf');
+  myFont = loadFont('https://raw.githubusercontent.com/OkayKenji/ZoomProject/master/Lato-Black.ttf');
 }
 
 function setup() {
   
   //change how it moves around the screen;
   ratex = Math.random();
-  ratey = Math.sqrt(1-ratex*ratex)
+  ratey = Math.sqrt(2-ratex*ratex)
   console.log(ratex);
   console.log(ratey);
   if (Math.random()<0.5)
@@ -30,26 +39,25 @@ function setup() {
   
   //creates the canvas and loads the font
   createCanvas(windowWidth, windowHeight);
-  textFont(myFont, 36);
+  textFont(myFont, 35);
   
   //sets intial text location as center
   x = windowWidth/2;
-  y =windowHeight/2;
+  y = windowHeight/2;
   
   //other presets that only have to be 
   //set once
   rectMode(CENTER);
   textAlign(CENTER);
+  
   fill(255, 255, 255);
-  textSize(120);
+  textSize(85);
 }
 
-
-
 function draw() {
-  background(35,35,36);
+  background("#232322");
 
-  text('Kenji', x, y);
+  text(nameToDisplay, x, y+25);
   
   //changes location
   if (addx)
@@ -64,23 +72,65 @@ function draw() {
   //prevents text from going off screen "bounce"
   //effect
   if (x<0) {
-    //x = windowWidth;
+    fill(random(small,255), random(small,255), random(small,255));
     addx = !addx; 
   }
   
   if (y<0) {
-    //y = windowHeight;
+    fill(random(small,255), random(small,255), random(small,255));
     addy = !addy; 
   }
 
   if (x>windowWidth) {
-    // x = 0;
+    fill(random(small,255), random(small,255), random(small,255));
     addx = !addx; 
   }
   
   if (y>windowHeight) {
-    //y = 0;
+    fill(random(small,255), random(small,255), random(small,255));
     addy = !addy;
   }
   
+  //hmmm Up,Up,Down,Down,Left,Left,Right,Right,b,a,enter
+  if (crickets) {
+    if (count%200<=100) {
+    textSize(40);
+    text('*crickets* *crickets* *crickets*', windowWidth/2, windowHeight-5);
+    textSize(85);
+    }
+    count++;
+  }
+}
+
+let match = ["ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowLeft", "ArrowRight", "ArrowRight", "b", "a", "Enter"];
+let arr = [];
+let counter = 0;
+
+function keyPressed() {
+  arr.push(key);
+
+  if (arr[counter]==match[counter]) {
+    //console.log("Match");
+    counter++;
+  } else  {
+    arr = [];
+    counter=0;
+  }
+  if (arr.length==11) {
+    //console.log("Win");
+    crickets = !crickets;
+    arr = []; 
+    counter=0;
+  } 
+
+  if (!crickets)
+    count = 0;
+   // arr = []; 
+
+  if (arr.length>11) {
+    counter=0;
+    arr = []; 
+  }
+ // console.log(arr);
+
 }
